8c5744e9278eed652ce65c2669b92f15169516a8
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2024-06-23 19:55:19 -0300

    Adicionando Scroll na Home

--- imhere/src/screens/Home/index.tsx
+++ imhere/src/screens/Home/index.tsx
@@ -1,39 +1,57 @@
-import { Text, View, TextInput, TouchableOpacity } from 'react-native';
+import {
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 
-import { styles } from './styles';
+import { styles } from "./styles";
+import { Participant } from "../../components/Participant";
 
 export function Home() {
+  const participants = [
+    "Bruno",
+    "Emanuele",
+    "Victor",
+    "Ana",
+    "Laura",
+    "Vera",
+    "Antonio",
+  ];
 
   function handleParticipantAdd() {
-    console.log('Botão adicionar')
+    console.log("Adicionar");
+  }
+
+  function handleParticipantRemove(name: string) {
+    console.log(`Remover ${name}`);
   }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.eventName}>
-        Nome do Evento
-      </Text>
+      <Text style={styles.eventName}>Nome do Evento</Text>
 
-      <Text style={styles.eventDate}>
-        Sexta, 4 de Novembro de 2024.
-      </Text>
-      <View 
-      style={styles.form}
-      >
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2024.</Text>
+      <View style={styles.form}>
         <TextInput
           style={styles.input}
-          placeholder='Nome do participante'
+          placeholder="Nome do participante"
           placeholderTextColor="#6b6b6b"
         />
-        <TouchableOpacity
-          style={styles.button}
-          onPress={handleParticipantAdd}
-        >
-          <Text style={styles.buttonText}>
-            +
-          </Text>
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>
       </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {participants.map((participant) => (
+          <Participant
+            key={participant}
+            name={participant}
+            onRemove={() => handleParticipantRemove("Bruno")}
+          />
+        ))}
+      </ScrollView>
     </View>
   );
 }
--- imhere/src/screens/Home/styles.ts
+++ imhere/src/screens/Home/styles.ts
@@ -1,47 +1,46 @@
-
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#131016',
-        padding: 24
-    },
-    eventName: {
-        color: '#FFF',
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginTop: 48
-    },
-    eventDate: {
-        color: '#6b6b6b',
-        fontSize: 16,
-    },
-    input: {
-        flex: 1,
-        height: 56,
-        backgroundColor: '#1F1E25',
-        borderRadius: 5,
-        color: '#FFF',
-        padding: 16,
-        marginRight: 10
-    },
-    buttonText: {
-        color: '#FFF',
-        fontSize: 24
-    },
-    button: {
-        width: 56,
-        height:56,
-        borderRadius: 5,
-        backgroundColor: '#31CF67',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    form: {
-        width: '100%',
-        flexDirection: 'row',
-        marginTop: 36,
-        marginBottom: 42
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "#131016",
+    padding: 24,
+  },
+  eventName: {
+    color: "#FFF",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48,
+  },
+  eventDate: {
+    color: "#6b6b6b",
+    fontSize: 16,
+  },
+  input: {
+    flex: 1,
+    height: 56,
+    backgroundColor: "#1F1E25",
+    borderRadius: 5,
+    color: "#FFF",
+    padding: 16,
+    marginRight: 10,
+  },
+  buttonText: {
+    color: "#FFF",
+    fontSize: 24,
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: "#31CF67",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  form: {
+    width: "100%",
+    flexDirection: "row",
+    marginTop: 36,
+    marginBottom: 42,
+  },
 });
