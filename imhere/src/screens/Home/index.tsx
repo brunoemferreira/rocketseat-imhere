3a9264055ea4224e18d4a6397e6369259a3830f0
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2024-06-25 21:30:03 -0300

    Removendo os participantes

--- imhere/src/screens/Home/index.tsx
+++ imhere/src/screens/Home/index.tsx
@@ -27,10 +27,13 @@ export function Home() {
   }
 
   function handleParticipantRemove(name: string) {
+     
+    
+
     Alert.alert("Remover", `Remover o participante ${name}?`, [
       {
         text: "Sim",
-        onPress: () => Alert.alert("Deletado"),
+        onPress: () => setParticipants(prevState => prevState.filter(participant =>participant !== name ))
       },
       {
         text: "Não",
